6df874dd92f3a015ebe1383f169ec3b87c8269fc
gonghaima@hotmail.com <5418854188>
2017-02-07 23:00:49 +1300

    api is ready on       /data/links

--- server.js
+++ server.js
@@ -6,12 +6,16 @@ let app=express();
 
 app.use(express.static('public'));
 
-app.listen(3000);
-
+let db;
 MongoClient.connect(MONGO_URL,(err, database)=>{
     if(err) throw err;
-    database.collection("links").find({}).toArray((err,links)=>{
+    db=database;
+    app.listen(3000, ()=>console.log('Listening on port 3000'));
+});
+
+app.get("/data/links",(req, res)=>{
+    db.collection("links").find({}).toArray((err,links)=>{
         if(err) throw err;
-        console.log(links);
+        res.json(links);
     });
 });
