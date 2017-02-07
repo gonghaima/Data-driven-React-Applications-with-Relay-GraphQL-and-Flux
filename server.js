5aeb29dcd95d5ac07c406b2f1ec59306526f6518
gonghaima@hotmail.com <5418854188>
2017-02-07 22:54:55 +1300

    MongoLab database created.

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import {MongoClient} from 'mongodb';
+import {MONGO_URL} from './secret';
 
 let app=express();
 
 app.use(express.static('public'));
 
 app.listen(3000);
+
+MongoClient.connect(MONGO_URL,(err, database)=>{
+    if(err) throw err;
+    database.collection("links").find({}).toArray((err,links)=>{
+        if(err) throw err;
+        console.log(links);
+    });
+});
