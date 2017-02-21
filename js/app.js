5e90aad08b4f330752ab2a7afed2c0749df79b9a
gonghaima@hotmail.com <5418854188>
2017-02-21 21:16:28 +1300

    Babel Relay Plugin - 1 - schema.json can be generated now.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Hello from './components/Hello';
 import Main from './components/Main';
+import Relay from 'react-relay';
 
 
 ReactDOM.render(<Main limit={4}/>, document.getElementById('react'));
+
+console.log(
+    Relay.QL`
+        {
+            links{
+                title
+            }
+        }
+    `
+);
--- server.js
+++ server.js
@@ -1,19 +1,31 @@
+import fs from 'fs';
 import express from 'express';
+import Schema from './data/schema';
 import {MongoClient} from 'mongodb';
 import {MONGO_URL} from './secret';
-import schema from './data/schema';
 import GraphQLHTTP from 'express-graphql';
+import {graphql} from 'graphql';
+import {introspectionQuery} from 'graphql/utilities';
 
 let app = express();
 
 app.use(express.static('public'));
 
-(async()=>{
+(async() => {
     let db = await MongoClient.connect(MONGO_URL);
-    app.use('/graphql', GraphQLHTTP({
-        schema: schema(db),
-        graphiql: true
-    }));
+    let schema = Schema(db);
+
+    app.use('/graphql', GraphQLHTTP({schema, graphiql: true}));
 
     app.listen(3000, () => console.log('Listening on port 3000'));
+
+    //Generate schema.json
+    let json = await graphql(schema, introspectionQuery);
+    fs.writeFile('./data/schema.json', JSON.stringify(json, null, 2), err => {
+        if (err) 
+            throw err;
+        console.log('JSON schema created');
+
+    });
+
 })();
