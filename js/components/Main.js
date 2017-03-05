bdd78ae928652798d2ac674a463b8c866b7bb2d8
gonghaima@hotmail.com <5418854188>
2017-03-05 13:43:52 +1300

    Relay - using a relay mutation

--- js/components/Main.js
+++ js/components/Main.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Relay from 'react-relay';
 import Link from './Link';
+import CreateLinkMutation from '../mutations/CreateLinkMutation';
 
 class Main extends Component {
     setLimit = (e) => {
@@ -9,6 +10,20 @@ class Main extends Component {
             .props
             .relay
             .setVariables({limit: newLimit});
+    }
+    handleSubmit=(e)=>{
+        e.preventDefault();
+        
+        Relay.Store.commitUpdate(
+            new CreateLinkMutation({
+                title:this.refs.newTitle.value,
+                url:this.refs.newUrl.value,
+                store:this.props.store
+            })
+        );
+        this.refs.newTitle.value="";
+        this.refs.newUrl.value="";
+
     }
     render() {
         let content = this
@@ -22,9 +37,15 @@ class Main extends Component {
         return (
             <div>
                 <h3>Links</h3>
-                <select onChange={this.setLimit} defaultValue="10">
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" placeholder="Title" ref="newTitle"/>
+                    <input type="text" placeholder="Url" ref="newUrl"/>
+                    <button type="text">Add</button>
+                </form>
+                <select onChange={this.setLimit} defaultValue={this.props.relay.variables.limit}>
                     <option value="5">5</option>
                     <option value="10">10</option>
+                    <option value="100">100</option>
                 </select>
                 <ul>
                     {content}
@@ -43,6 +64,7 @@ Main = Relay.createContainer(Main, {
         store: () => Relay
             .QL `
             fragment on Store{
+                id,
                 linkConnection(first:$limit){
                     edges{
                         node{
